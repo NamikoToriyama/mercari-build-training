bd75af7b500c7b45bdb2d8e6da99db083dbdc940
NamikoToriyama <torichan@mercari.com>
2024-02-19 13:53:16 +0900

    add step3-5

--- go/app/main.go
+++ go/app/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -14,7 +19,8 @@ import (
 )
 
 const (
-	ImgDir = "images"
+	ImgDir        = "images"
+	ItemsJsonPath = "./../db/items.json"
 )
 
 type Response struct {
@@ -22,8 +28,9 @@ type Response struct {
 }
 
 type Item struct {
-	Name     string `json:"name"`
-	Category string `json:"category"`
+	Name          string `json:"name"`
+	Category      string `json:"category"`
+	ImageFilename string `json:imageFilename`
 }
 
 type Items struct {
@@ -37,7 +44,7 @@ func root(c echo.Context) error {
 
 func listItems() (*Items, error) {
 	// jsonファイルの読み込み
-	file, err := os.OpenFile("./items.json", os.O_RDWR|os.O_CREATE, 0644) // os.openでも良い。書き込み権限があるか確認
+	file, err := os.OpenFile(ItemsJsonPath, os.O_RDWR|os.O_CREATE, 0644) // os.openでも良い。書き込み権限があるか確認
 	if err != nil {
 		return nil, fmt.Errorf("Error opening file:", err)
 	}
@@ -52,6 +59,40 @@ func listItems() (*Items, error) {
 	return &currentItems, nil
 }
 
+func hashString(input string) string {
+	// https://pkg.go.dev/crypto/sha256
+	hash := sha256.New()
+	hash.Write([]byte(input))
+	hashed := hash.Sum(nil)
+	// 文字列にする
+	return hex.EncodeToString(hashed)
+}
+
+func storeImage(image *multipart.FileHeader, imageName string) error {
+	// 取得した画像をファイルに保存
+	file, err := image.Open()
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Create a new file for the uploaded image
+	filePath := path.Join(ImgDir, imageName+".jpg")
+	out, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Copy the uploaded image data to the new file
+	_, err = io.Copy(out, file)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // add items to json
 // {"items": [{"name": "jacket", "category": "fashion"}, ...]}
 // point: errorの処理をちゃんとやること
@@ -60,12 +101,11 @@ func addItemToJson(c echo.Context, item *Item) error {
 	if err != nil {
 		return err
 	}
-
 	// itemを追加
 	currentItems.Items = append(currentItems.Items, *item)
 
 	// 書き込み用にファイルを開く
-	file, err := os.Create("./items.json")
+	file, err := os.Create(ItemsJsonPath)
 	if err != nil {
 		return fmt.Errorf("Error opening file for writing:", err)
 	}
@@ -83,17 +123,31 @@ func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
 	category := c.FormValue("category")
+	image, err := c.FormFile("image")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("failed c.FormFile: %s", err.Error()))
+	}
+
+	// ファイル名をhash化
+	hashedImageName := hashString(image.Filename)
+
+	// 画像として保存
+	if err := storeImage(image, hashedImageName); err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("failed storeImage: %s", err.Error()))
+	}
+
 	c.Logger().Infof("Receive item: %s category: %s", name, category)
 	item := &Item{
-		Name:     name,
-		Category: category,
+		Name:          name,
+		Category:      category,
+		ImageFilename: hashedImageName,
 	}
 
 	if err := addItemToJson(c, item); err != nil {
 		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 
-	message := fmt.Sprintf("item received: %s \n category received: %s", name, category)
+	message := fmt.Sprintf("item received: %s category received: %s image received: %s", name, category, hashedImageName)
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
@@ -102,10 +156,27 @@ func addItem(c echo.Context) error {
 func getItems(c echo.Context) error {
 	currentItems, err := listItems()
 	if err != nil {
-		return err
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 
 	return c.JSON(http.StatusOK, currentItems)
+}
+
+func getItem(c echo.Context) error {
+	items, err := listItems()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("failed GetItems: %s", err.Error()))
+	}
+
+	str_id := c.Param("id")
+	// 文字列を整数に変換
+	id, err := strconv.Atoi(str_id)
+	fmt.Println("Error opening file:", id, len(items.Items))
+	if err != nil || id < 0 || len(items.Items) < id {
+		return c.JSON(http.StatusBadRequest, "invalid id")
+	}
+
+	return c.JSON(http.StatusOK, items.Items[id-1])
 }
 
 func getImg(c echo.Context) error {
@@ -144,6 +215,7 @@ func main() {
 	e.GET("/", root)
 	e.POST("/items", addItem)
 	e.GET("/items", getItems)
+	e.GET("/items/:id", getItem)
 	e.GET("/image/:imageFilename", getImg)
 
 	// Start server
