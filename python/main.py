32f39f2afd9acc6f2188590848202039194e53ed
NamikoToriyama <torichan@mercari.com>
2025-03-07 12:04:50 +0900

    add sql and update main.py

--- python/main.py
+++ python/main.py
@@ -1,15 +1,85 @@
 import os
 import logging
 import pathlib
-import json
-import hashlib
-from fastapi import FastAPI, Form, HTTPException, File, UploadFile
+from fastapi import FastAPI, Form, HTTPException, Depends, UploadFile, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from typing import Dict, List
 
-app = FastAPI()
+
+# Define the path to the images & sqlite3 database
+images = pathlib.Path(__file__).parent.resolve() / "images"
+db = pathlib.Path(__file__).parent.resolve() / "db" / "mercari.sqlite3"
+
+
+def get_db():
+    if not db.exists():
+        yield
+
+    conn = sqlite3.connect(db)
+    conn.row_factory = sqlite3.Row  # Return rows as dictionaries
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+################## for STEP 4-1: ####################
+import json
+JSON_FILE = 'items.json'
+# Function to read the JSON file
+def read_json_file() -> Dict[str, List[Dict[str,str]]]:
+    if not os.path.exists(JSON_FILE):
+        with open(JSON_FILE, 'w') as f:
+            json.dump({"items": []}, f)  # Initialize with an empty list
+    with open(JSON_FILE, 'r') as f:
+        return json.load(f)
+
+# Function to write data to the JSON file
+def write_json_file(data):
+    with open(JSON_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
+
+######################################################
+
+################## for STEP 4-4: #####################
+import hashlib
+def hash_image(image_file: UploadFile) -> str:
+    try:
+        # Read image
+        image = image_file.file.read()
+        hash_value = hashlib.sha256(image).hexdigest()
+        hashed_image_name = f"{hash_value}.jpg"
+        hashed_image_path = images / hashed_image_name
+        # Save image with hashed value as image name
+        with open(hashed_image_path, 'wb') as f:
+            f.write(image)
+        return hashed_image_name
+    
+    except Exception as e:
+        raise RuntimeError(f"An unexpected error occurred: {e}")
+
+######################################################
+
+# STEP 5-1: set up the database connection
+def setup_database():
+    pass
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    setup_database()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 logger = logging.getLogger("uvicorn")
-logger.level = logging.INFO
+# For STEP 4-6
+logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
@@ -20,60 +90,56 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-JSON_PATH = '../db/items.json'
-IMAGE_DIR = '../images/'
 
-def load_json_to_dict():
-    with open(JSON_PATH, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
-
-def write_dict_to_json(data_dict):
-    with open(JSON_PATH, 'w', encoding='utf-8') as json_file:
-        json.dump(data_dict, json_file, ensure_ascii=False, indent=4)
+class HelloResponse(BaseModel):
+    message: str
 
 
-def hash_string_sha256(input_string):
-    sha256_hash = hashlib.sha256()
-    sha256_hash.update(input_string.encode('utf-8'))
-    return sha256_hash.hexdigest()
-
-def save_image(image: UploadFile):
-   filename = f"{hash_string_sha256(image.filename)}.png"
-   with open(f"{images}/{filename}" , "wb") as buffer:
-        buffer.write(image.file.read())
-   return filename
-
-@app.get("/")
-def root():
-    return {"message": "Hello, world!"}
+@app.get("/", response_model=HelloResponse)
+def hello():
+    return HelloResponse(**{"message": "Hello, world!"})
 
 
-@app.post("/items")
-def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = File(...)):
-    logger.info(f"Receive item: {name}")
-    hashed_name = save_image(image)
-    data = load_json_to_dict()['items']
-    data.append({"name": name, "category": category, "image": hashed_name})
-    write_dict_to_json(data)
-    return {"message": f"item received: {name}"}
+class AddItemResponse(BaseModel):
+    message: str
+
+
+# add_item is a handler to add a new item for POST /items .
+@app.post("/items", response_model=AddItemResponse)
+def add_item(
+    name: str = Form(...),
+    category: str = Form(...), # For STEP 4-2
+    image: UploadFile = File(...), # For STEP 4-4
+    db: sqlite3.Connection = Depends(get_db),
+):
+    if not name:
+        raise HTTPException(status_code=400, detail="name is required")
+    # for STEP 4-2
+    if not category:
+        raise HTTPException(status_code=400, detail="category is required")
+    # for STEP 4-4
+    if not image:
+        raise HTTPException(status_code=400, detail="image is required")
+
+    hashed_image = hash_image(image)
+    insert_item(Item(name=name, category=category, image=hashed_image))
+    return AddItemResponse(**{"message": f"item received: {name}"})
 
 @app.get("/items")
-def list_items():
-    logger.info(f"list items")
-    data = load_json_to_dict()
-    return {"items": data}
+def get_items():
+    # For STEP 4-3
+    all_data = read_json_file()
+    return all_data
 
+# For STEP 4-5
 @app.get("/items/{item_id}")
-def get_item(item_id):
-    logger.info(f"get item")
-    id = int(item_id)-1
-    try:
-        item = load_json_to_dict()[id]
-    except Exception as e:
-        raise ValueError(f"item not found item_id = {id}; error = {e}")
+def get_item_by_id(item_id):
+    item_id_int = int(item_id)
+    all_data = read_json_file()
+    item = all_data["items"][item_id_int - 1]
     return item
 
+# get_image is a handler to return an image for GET /images/{filename} .
 @app.get("/image/{image_name}")
 async def get_image(image_name):
     # Create image path
@@ -83,7 +149,27 @@ async def get_image(image_name):
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
     if not image.exists():
-        logger.info(f"Image not found: {image}")
+        logger.debug(f"Image not found: {image}")
         image = images / "default.jpg"
 
     return FileResponse(image)
+
+
+class Item(BaseModel):
+    name: str
+    category: str
+    image: str
+
+
+def insert_item(item: Item):
+    current_data = read_json_file()
+
+    ################################ for STEP 4-2 ################################
+    # current_data["items"].append({"name": item.name, "category": item.category})
+    #############################################################################
+
+    ################################ for STEP 4-4 ################################
+    current_data["items"].append({"name": item.name, "category": item.category, "image_name": item.image})
+    #############################################################################
+    
+    write_json_file(current_data)
